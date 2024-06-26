1c75eb26122690e6c7dc198c5c6b3d56e5b2c8e8
MayraFazzari <mayrafazzariclerici.01@gmail.com>
2024-06-25 23:09:20 -0300

    filtros por fecha admin/ bdd actualizada

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -15,46 +15,58 @@ class AdminController
     }
 
     public function get (){
-        $this->mostrarDatos();
+        $this->mostrarDatosPorFechas();
     }
 
 
-    public function mostrarDatos()
-    {
-        $totalUsuarios = $this->model->getTotalUsuarios();
-        $preguntasTotales= $this->model->getCantidadPreguntas();
-        $contadorUsuariosPais = $this->model->getUsuariosPorPais();
-        $usuariosMenores=$this->model->usuariosMenores();
-        $usuariosAdultos=$this->model->usuariosAdultos();
-        $usuariosMayores=$this->model->usuariosMayores();
-        $nuevosUsuarios=$this->model->nuevosUsuarios();
-        $usuariosGeneroF=$this->model->usuariosGeneroF();
-        $usuariosGeneroM=$this->model->usuariosGeneroM();
-        $preguntasCreadas=$this->model->preguntasCreadas();
-        $partidasClasicas=$this->model->partidasClasicas();
-        $partidasDuelo=$this->model->partidasDuelo();
-        $porcentajeRespUser=$this->model->porcentajeRespUser();
 
-        $data = array(
-            "cantidad_usuarios" => $totalUsuarios,
-            "preguntasTotales"=>$preguntasTotales,
-            "contadorUsuariosPais"=>$contadorUsuariosPais,
-            "usuariosMenores"=>$usuariosMenores,
-            "usuariosAdultos"=>$usuariosAdultos,
-            "usuariosMayores"=>$usuariosMayores,
-            "nuevosUsuarios"=>$nuevosUsuarios,
-            "usuariosGeneroF"=>$usuariosGeneroF,
-            "usuariosGeneroM"=>$usuariosGeneroM,
-            "preguntasCreadas"=>$preguntasCreadas,
-            "partidasClasicas"=>$partidasClasicas,
-            "partidasDuelo"=>$partidasDuelo,
-            "porcentajeRespUser"=>$porcentajeRespUser,
-        );
+//-----------------------------------------------------------------------------------*/
+    public function mostrarDatosPorFechas()
+    {
+        $data = array();
+        if (isset($_POST['fechaInicio']) && isset($_POST['fechaFin'])) {
+            $fechaInicio = $_POST['fechaInicio'];
+            $fechaFin = $_POST['fechaFin'];
+
+            // Obtener datos según el rango de fechas
+            $preguntasTotales = $this->model->getPreguntasTotales($fechaInicio, $fechaFin);
+            $totalUsuarios = $this->model->getTotalUsuarios($fechaInicio, $fechaFin);
+            $contadorUsuariosPais = $this->model->getUsuariosPorPais($fechaInicio,$fechaFin);
+            $usuariosMenores = $this->model->usuariosMenores($fechaInicio, $fechaFin);
+            $usuariosAdultos = $this->model->usuariosAdultos($fechaInicio, $fechaFin);
+            $usuariosMayores = $this->model->usuariosMayores($fechaInicio, $fechaFin);
+            $nuevosUsuarios = $this->model->nuevosUsuarios($fechaInicio, $fechaFin);
+            $usuariosGeneroF = $this->model->usuariosGeneroF($fechaInicio, $fechaFin);
+            $usuariosGeneroM = $this->model->usuariosGeneroM($fechaInicio, $fechaFin);
+            $preguntasCreadas = $this->model->preguntasCreadas($fechaInicio, $fechaFin);
+            $partidasClasicas = $this->model->partidasClasicas($fechaInicio, $fechaFin);
+            $partidasDuelo = $this->model->partidasDuelo($fechaInicio, $fechaFin);
+            $porcentajeRespUser = $this->model->porcentajeRespUser($fechaInicio, $fechaFin);
+
+
+            $data = array(
+                "preguntasTotales" => $preguntasTotales,
+                "cantidad_usuarios" => $totalUsuarios,
+                "contadorUsuariosPais" => $contadorUsuariosPais,
+                "usuariosMenores" => $usuariosMenores,
+                "usuariosAdultos" => $usuariosAdultos,
+                "usuariosMayores" => $usuariosMayores,
+                "nuevosUsuarios" => $nuevosUsuarios,
+                "usuariosGeneroF" => $usuariosGeneroF,
+                "usuariosGeneroM" => $usuariosGeneroM,
+                "preguntasCreadas" => $preguntasCreadas,
+                "partidasClasicas" => $partidasClasicas,
+                "partidasDuelo" => $partidasDuelo,
+                "porcentajeRespUser" => $porcentajeRespUser,
+            );
+        }
+
+
         $this->presenter->render('homeAdmin', $data);
     }
 
 
-
+   //--------------------------------------------------------------------------------------------*/
     public function generarPDF()
     {
         // Obtener datos necesarios para el PDF desde el modelo
@@ -105,7 +117,6 @@ class AdminController
         $template = $mustache->loadTemplate($viewName . '.mustache');
         return $template->render($data);
     }
-
 
 
 
--- model/AdminModel.php
+++ model/AdminModel.php
@@ -8,69 +8,75 @@ class AdminModel
         $this->database = $database;
     }
 
-    public function getTotalUsuarios()
+    public function getTotalUsuarios($fechaInicio,$fechaFin)
     {
-        $query = "SELECT COUNT(*) as total_usuarios FROM login";
+        $query = "SELECT COUNT(*) as total_usuarios FROM datos_usuario WHERE fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
 
     }
 
-    public function getCantidadPreguntas(){
-        $query = "SELECT COUNT(id_pregunta) AS preguntasTotales FROM preguntas_listado";
+    public function getPreguntasTotales($fechaInicio, $fechaFin)
+    {
+        $query = "SELECT COUNT(id_pregunta) AS preguntasTotales FROM preguntas_listado WHERE fecha_creacion BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
+
     }
 
-    public function getUsuariosPorPais(){
-        $query = "SELECT COUNT(id_usuario) AS contadorUsuarios, pais FROM datos_usuario
+
+    public function getUsuariosPorPais($fechaInicio,$fechaFin){
+        $query = "SELECT COUNT(id_usuario) AS contadorUsuarios, pais FROM datos_usuario WHERE fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'
                                            GROUP BY pais";
         return $this->database->query($query);
     }
 
-    public function usuariosMenores(){
-        $query = "SELECT COUNT(id_usuario) AS usuariosMenores FROM datos_usuario WHERE YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) < 18 AND YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) > 0";
+    public function usuariosMenores($fechaInicio,$fechaFin){
+        $query = "SELECT COUNT(id_usuario) AS usuariosMenores FROM datos_usuario WHERE YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) < 18 AND YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) > 0
+                  AND fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
     }
 
-    public function usuariosAdultos(){
-        $query = "SELECT COUNT(id_usuario) AS usuariosAdultos FROM datos_usuario WHERE YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) >= 18 AND YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) <= 64";
+    public function usuariosAdultos($fechaInicio,$fechaFin){
+        $query = "SELECT COUNT(id_usuario) AS usuariosAdultos FROM datos_usuario WHERE YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) >= 18 AND YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) <= 64
+                  AND fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
     }
 
-    public function usuariosMayores(){
-        $query = "SELECT COUNT(id_usuario) AS usuariosMayores FROM datos_usuario WHERE YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) >= 65";
+    public function usuariosMayores($fechaInicio,$fechaFin){
+        $query = "SELECT COUNT(id_usuario) AS usuariosMayores FROM datos_usuario WHERE YEAR(CURDATE()) - YEAR(nacimiento) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(nacimiento, '%m%d')) >= 65
+                  AND fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
     }
-    public function nuevosUsuarios(){
-        $query = "SELECT COUNT(id_usuario) AS nuevosUsuarios FROM datos_usuario WHERE fecha_registro >= CURDATE()";
+    public function nuevosUsuarios($fechaInicio,$fechaFin){
+        $query = "SELECT COUNT(id_usuario) AS nuevosUsuarios FROM datos_usuario WHERE fecha_registro >= CURDATE() AND fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
     }
 
-    public function usuariosGeneroF(){
-        $query="SELECT COUNT(*) AS usuariosGeneroF FROM datos_usuario WHERE genero = 'F';";
+    public function usuariosGeneroF($fechaInicio,$fechaFin){
+        $query="SELECT COUNT(*) AS usuariosGeneroF FROM datos_usuario WHERE genero = 'F'AND fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'";
             return $this->database->query($query);
     }
 
-    public function usuariosGeneroM(){
-        $query="SELECT COUNT(*) AS usuariosGeneroM FROM datos_usuario WHERE genero = 'M';";
+    public function usuariosGeneroM($fechaInicio,$fechaFin){
+        $query="SELECT COUNT(*) AS usuariosGeneroM FROM datos_usuario WHERE genero = 'M' AND fecha_registro BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
     }
 
-    public function preguntasCreadas(){
-        $query="SELECT COUNT(*) AS preguntasCreadas FROM preguntas_sugeridas;";
+    public function preguntasCreadas($fechaInicio,$fechaFin){
+        $query="SELECT COUNT(*) AS preguntasCreadas FROM preguntas_sugeridas WHERE fecha BETWEEN '$fechaInicio' AND '$fechaFin'";
         return $this->database->query($query);
     }
 
-    public function partidasClasicas(){
-        $query="SELECT COUNT(*) AS partidasClasicas FROM partida_clasica_general;";
+    public function partidasClasicas($fechaInicio,$fechaFin){
+        $query="SELECT COUNT(*) AS partidasClasicas FROM partida_clasica_general WHERE fecha BETWEEN '$fechaInicio' AND '$fechaFin';";
         return $this->database->query($query);
     }
 
-    public function partidasDuelo(){
-        $query="SELECT COUNT(*) AS partidasDuelo FROM partida_duelo_general;";
+    public function partidasDuelo($fechaInicio,$fechaFin){
+        $query="SELECT COUNT(*) AS partidasDuelo FROM partida_duelo_general WHERE fecha BETWEEN '$fechaInicio' AND '$fechaFin';";
         return $this->database->query($query);
     }
 
-    public function porcentajeRespUser() {
+    public function porcentajeRespUser($fechaInicio,$fechaFin) {
         $query = "SELECT du.id_usuario, du.nombre AS nombre_usuario, du.imagen_perfil,COUNT(pr.acertado = 1 OR NULL) AS respuestas_correctas,
                 COUNT(pr.id_jugador) AS total_respuestas, ROUND((COUNT(pr.acertado = 1 OR NULL) / COUNT(pr.id_jugador)) * 100, 2) AS porcentaje_correctas
                 FROM partida_clasica_respuestas pr 
@@ -78,13 +84,13 @@ class AdminModel
                 INNER JOIN datos_usuario du ON pr.id_jugador = du.id_usuario
                 LEFT JOIN respuesta_listado rl ON pr.id_pregunta = rl.id_pregunta AND pr.acertado = rl.correcta
                 WHERE pg.hora_final != '0000-00-00 00:00:00'  -- Excluir partidas incompletas
-                GROUP BY du.id_usuario, du.nombre, du.imagen_perfil  
-                ORDER BY porcentaje_correctas DESC";
+                 AND pg.fecha BETWEEN '$fechaInicio' AND '$fechaFin' 
+                GROUP BY du.id_usuario, du.nombre, du.imagen_perfil  ORDER BY 
+porcentaje_correctas DESC";
 
         return $this->database->query($query);
     }
 
 }
-
 ?>
 
